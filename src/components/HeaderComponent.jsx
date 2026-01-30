09de8b254ffb5a79eb623b7524896269d4648679
rober <rgavhur120@g.educaand.es>
2026-01-30 23:47:20 +0100

    feat: logo

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -10,7 +10,7 @@ function HeaderComponent() {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
 
-          <label onClick={() => window.location.reload()} className="navbar-brand">SGFAB</label>
+          <label onClick={() => window.location.reload()} className="navbar-brand"><img width={80} src="/logoFloristeria.png" alt="logo floristería" /></label>
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
